98f67cad4c40831c96761549f6039c2dfe76f9e1
Theodore Baschak <theodore@ciscodude.net>
2017-02-03 20:54:55 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ipaddr.ca-python
 python rewrite of ipaddr.ca
+
+## Requirements:
+
+*	python-virtualenv python-pip uwsgi
+
+## Instructions
+
+	cd /home/ipaddr
+	git clone https://github.com/tbaschak/ipaddr.ca-python
+	cd ipaddr.ca-python
+	virtualenv ipaddrenv
+	source ipaddrenv/bin/activate
+	pip install flask uwsgi
+
+Afterwards setup this application like any other uwsgi/flask/python application, for a refresher visit [this](https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uwsgi-and-nginx-on-ubuntu-16-04) handy DigitalOcean Community Tutorial. There is an ini file included for uwsgi and a nginx configuration vhost.
+
+
---
+++ ipaddrca.py
@@ -0,0 +1,77 @@
+import os
+import json
+from flask import Flask, request, jsonify, Response
+from pierky.ipdetailscache import IPDetailsCache
+
+cache = IPDetailsCache()
+cache.UseIXPs(WhenUse=2)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "<h1 style='color:salmon'>Hello There!!!!</h1>"
+
+@app.route("/ip", methods=["GET"])
+def simple_ip():
+    resp = Response(request.remote_addr)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
+
+@app.route("/ptr", methods=["GET"])
+def simple_ptr():
+    result = cache.GetIPInformation(request.remote_addr)
+    try:
+        ptr = result["HostName"]
+    except NameError:
+        ptr = "none"
+    resp = Response(ptr)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
+
+@app.route("/api/ip", methods=["GET"])
+def json_ipaddr_ip():
+    resp = jsonify({'ip': request.remote_addr})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
+
+@app.route("/api/ptr", methods=["GET"])
+def json_ipaddr_ptr():
+    result = cache.GetIPInformation(request.remote_addr)
+    try:
+        ptr = result["HostName"]
+    except NameError:
+        ptr = "none"
+    resp = jsonify({'ptr': ptr })
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
+
+@app.route("/api/v1/bgp/", methods=["GET","POST"])
+def get_bgp_myip():
+    if request.method == "POST":
+        ip = request.form["text"]
+    else:
+        ip = request.remote_addr
+    result = cache.GetIPInformation(ip)
+    resp = jsonify(result)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
+
+@app.route("/api/v1/bgp/<ip>", methods=["GET","POST"])
+def get_bgp_ip(ip):
+    result = cache.GetIPInformation(ip)
+    resp = jsonify(result)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET'
+    resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,Accept,Content-Type,Origin'
+    return resp
